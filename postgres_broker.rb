7a20f80bccc632c1ddc192d419d169d197cc61c8
cskksc <ckoparkar@live.in>
2015-02-09 22:22:27 +0530

    Initial config.

--- postgres_broker.rb
+++ postgres_broker.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
+require 'json'
+require 'yaml'
 
 class PostgresBroker < Sinatra::Base
   use Rack::Auth::Basic do |username, password|
     credentials = {username: 'admin', password: 'admin'}
-    username == credentials.fetch(:username) and password == credentials.fetch(:npassword)
+    username == credentials.fetch(:username) and password == credentials.fetch(:password)
   end
 
   get '/' do
     "hello world"
+  end
+
+  get '/v2/catalog' do
+    content_type :json
+    YAML.load_file('config/settings.yml').fetch('catalog').to_json
   end
 end
