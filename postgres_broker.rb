7368ed81350f6f288f168222e78c5432a4837049
cskksc <ckoparkar@live.in>
2015-02-09 21:53:45 +0530

    Setup basic authentication.

--- postgres_broker.rb
+++ postgres_broker.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 
 class PostgresBroker < Sinatra::Base
+  use Rack::Auth::Basic do |username, password|
+    credentials = {username: 'admin', password: 'admin'}
+    username == credentials.fetch(:username) and password == credentials.fetch(:npassword)
+  end
+
   get '/' do
     "hello world"
   end
