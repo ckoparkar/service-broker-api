a67243875bbd4447175c5cc2226c4b75ad2024a7
cskksc <ckoparkar@live.in>
2015-02-10 10:32:17 +0530

    Make broker credentials configurable.

--- postgres_broker.rb
+++ postgres_broker.rb
@@ -1,19 +1,24 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require 'yaml'
 
 class PostgresBroker < Sinatra::Base
   use Rack::Auth::Basic do |username, password|
-    credentials = {username: 'admin', password: 'admin'}
-    username == credentials.fetch(:username) and password == credentials.fetch(:password)
+    credentials = self.app_settings.fetch('basic_auth')
+    username == credentials.fetch('username') and password == credentials.fetch('password')
   end
-
   get '/' do
     "hello world"
   end
 
   get '/v2/catalog' do
     content_type :json
-    YAML.load_file('config/settings.yml').fetch('catalog').to_json
+    self.class.app_settings.fetch('catalog').to_json
+  end
+
+  private
+
+  def self.app_settings
+    YAML.load_file('config/settings.yml')
   end
 end
