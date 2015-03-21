83ab2d48d39f59b1d35460164efd0a38a182c086
cskksc <ckoparkar@live.in>
2015-03-21 10:52:27 +0530

    Duplicate service instance creation returns 409.

--- features/provision.feature
+++ features/provision.feature
@@ -3,4 +3,11 @@ Feature: As a user I can provision service instances
   Scenario: I can create a new service instance
     When I send PUT request to "/v2/service_instances/:id" with the following:
     | id | 1 |
+    And I dont get any errors
     Then the response status should be "201"
+
+  Scenario: I try to create a duplicate service instance
+    When I send PUT request to "/v2/service_instances/:id" with the following:
+    | id | 1 |
+    And it raises "DatabaseAlreadyExistsError"
+    Then the response status should be "409"
--- features/step_definitions/sinatra_steps.rb
+++ features/step_definitions/sinatra_steps.rb
@@ -1,13 +1,3 @@
-Before do
-  postgresql_service = double
-  allow(PostgresHelper).to receive(:new).with({"host"=>"localhost",
-                                               "username"=>"admin",
-                                               "password"=>"admin",
-                                               "port"=>0}).and_return(postgresql_service)
-
-  allow(postgresql_service).to receive(:create_database).with('d1').and_return('http://127.0.0.1:5432/databases/mydb')
-end
-
 Then(/^I should see "(.*?)"$/) do |text|
   expect(last_response).to match text
 end
@@ -25,11 +15,34 @@ When /^I send (GET|POST|PUT|DELETE) request (?:for|to) "([^"]*)"(?: with the fol
     end
   end
 
+  z = path.clone
   request_opts[:params].each_pair do |k, v|
     x = path =~ /:#{Regexp.quote(k)}/
     y = x + k.length
-    path[x..y] = v
+    z[x..y] = v
   end
 
-  eval "#{request_type.downcase} '#{path}'"
+  @req = "#{request_type.downcase} '#{z}'"
+end
+
+And(/I dont get any errors/) do
+    postgresql_service = double
+    expect(PostgresHelper).to receive(:new).with({"host"=>"localhost",
+                                               "username"=>"admin",
+                                               "password"=>"admin",
+                                               "port"=>0}).and_return(postgresql_service)
+    expect(postgresql_service).to receive(:create_database).with('d1').and_return('http://127.0.0.1:5432/databases/mydb')
+    eval @req
+end
+
+When(/^it raises "(.*?)"$/) do |error|
+  postgresql_service = double
+  expect(PostgresHelper).to receive(:new).with({"host"=>"localhost",
+                                               "username"=>"admin",
+                                               "password"=>"admin",
+                                               "port"=>0}).and_return(postgresql_service)
+
+  expect(postgresql_service).to receive(:create_database).with('d1').and_raise(Kernel.const_get error)
+
+  eval @req
 end
