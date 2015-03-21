d6d075e9dd549a230dc5e876d9e634b15e52a537
cskksc <ckoparkar@live.in>
2015-03-21 15:52:07 +0530

    Refactor get request step_definition.

--- features/step_definitions/sinatra_steps.rb
+++ features/step_definitions/sinatra_steps.rb
@@ -6,19 +6,8 @@ Then(/^I should see "(.*?)"$/) do |text|
   expect(last_response).to match text
 end
 
-When /^I send GET request (?:for|to) "([^"]*)"(?: with the following:)?$/ do |*args|
-  request_type = args.shift
-  path = args.shift
-  input = args.shift
-  request_opts = {method: request_type.downcase.to_sym}
-  unless input.nil?
-    if input.class == Cucumber::Ast::Table
-      request_opts[:params] = input.rows_hash
-    else
-      request_opts[:params] = eval input
-    end
-  end
-  request path
+When /^I send GET request to "([^"]*)"/ do |path|
+  get path
 end
 
 When /^I send (PUT|DELETE) request (?:for|to) "([^"]*)"(?: with the following:)?$/ do |*args|
